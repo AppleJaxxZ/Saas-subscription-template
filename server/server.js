442af9a8040990dd2df39b45d93fd0950e1d5006
Alex Zajacek <smackanalex@gmail.com>
2022-03-30 15:31:58 -0400

    Changes

---
+++ server/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { readdirSync } from "fs";
+
+const morgan = require("morgan");
+require("dotenv").config();
+
+const app = express();
+
+// db
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log("DB Connection Error ", err));
+
+// middlewares
+app.use(express.json({ limit: "5mb" }));
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL],
+  })
+);
+
+// autoload routes
+app.get("/api/register", (req, res) => {
+  res.send("Hey you reached node server");
+});
+
+// listen
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
